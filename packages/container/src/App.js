dccf6548e03bebb72d7761bd0cc9ed5169d50fd0
Anderson Luís Furlan <alsfurlan@gmail.com>
2023-05-12 19:13:03 -0300

    Avoid style conflict between the applications

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,14 +1,24 @@
 import Header from './components/Header';
 import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
 
 export default () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
